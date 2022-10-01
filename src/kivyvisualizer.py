5404e837aa4864eb2727d75c2b8bc33719e94ef0
aMetallurgist <joshua.d1434@gmail.com>
2022-10-01 08:52:13 -0700

    Update kv editor to use CodeInput widget Default lexer is Python, which provides nice syntax highlighting

--- src/kivyvisualizer.py
+++ src/kivyvisualizer.py
@@ -1,15 +1,9 @@
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.codeinput import CodeInput
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.lang import Builder
-
-class KivyEditor(TextInput):
-    # Note: I can use extras\highlight.py to syntax highlight the editor text!!
-    # Actualy codeinput.py provides this 'out of the box' functionality 
-    def __init__(self):
-        super().__init__()
 
 class KivyVisualizer(BoxLayout):
     # kivy's parser.py has a 'proxyapp'. Can I use this approach instead of choosing to insert the root widget?
@@ -17,7 +11,7 @@ class KivyVisualizer(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.editor = KivyEditor()
+        self.editor = CodeInput(do_wrap=False)
         self.visualizer = BoxLayout()
         self.editor.bind(text=self.handle_kv_change)
         self.add_widget(self.editor)
