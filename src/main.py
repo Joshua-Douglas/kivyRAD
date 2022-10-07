ba5b1a9c2e448802f5784a955a23c76c374a1972
aMetallurgist <joshua.d1434@gmail.com>
2022-10-07 14:29:31 -0700

    KivyDesignerApp supports child visualization Update KivyDesignerApp to support spawning a child process to visualize hot reloads. The hot reloader currently only supports kvlang changes, and breaks when updates to the actual application are made. The HotReloadApp needs to be updated to redirect the running app to the visualized app.
    Also, add smoother handling of window exits for both the parent and
    child process. The instruction listening task now has its own
    instruction set, that can be used to stop listening.

--- src/main.py
+++ src/main.py
@@ -2,78 +2,92 @@ import os
 from pathlib import Path
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.clock import Clock
 from kivy.factory import Factory 
 from kivy.uix.boxlayout import BoxLayout
+
+import multiprocessing
+from visualizationsubprocess import VisualizationSubprocess
 
 SRC_DIRECTORY = Path(os.path.dirname(__file__))
 DATA_FOLDER = os.path.join(SRC_DIRECTORY.parent, 'data') 
 ICON_PATH = os.path.join(DATA_FOLDER, 'kivy-icon-48.png')
 
-from kivy.core.window import Window
-
-
 class RootWidget(BoxLayout):
     pass
 
 class KivyDesignerApp(App):
+    '''
+    The main application. 
+    Provides a range of tools to allow users to build 
+    and visualize kivy applications in real time. To 
+    maximize accuracy, the kivy application is visualized
+    in a child process. This process currently runs with
+    the same version of python and kivy as the KivyDesigner, 
+    and does not provide any options for configuring the 
+    kivy environment. 
+
+    Updates to the visualized applications are triggered 
+    by sending a HotReloadInstruction to the child process,
+    through a multiprocessing Queue.  
+    '''
     def build(self):
         self.title = 'Kivy Designer'
+        self.visualization_instructions = multiprocessing.Queue()
+        self.visualization_subprocess = None
         root_widget = Builder.load_file(os.path.join(SRC_DIRECTORY, 'KivyDesigner.kv'))
         return root_widget
 
     def on_stop(self):
-        '''Clean up application resources, such as Watchdog observer.
         '''
-        pass
+        Gracefully terminate the visualization subprocess when the kivy 
+        designer is stopped.
+        '''
+        if self._is_visualizing():
+            self.visualization_subprocess.terminate()
+
+    def _is_visualizing(self):
+        '''
+        Return true if a kivy visualizer subprocess is running. 
+        Return false otherwise.
+        '''
+        result = self.visualization_subprocess is not None
+        if result:
+            try:
+                result = self.visualization_subprocess.is_alive()
+            except ValueError as e:
+                # is_alive will throw a ValueError if the process is 
+                # already closed
+                result = False
+
+        # We know the subprocess is no longer valid. Clear our ref
+        if not result and self.visualization_subprocess:
+            self.visualization_subprocess = None 
+        return result
+
+    def _start_visualizing(self):
+        '''
+        Run the VisualizationSubprocess on a new child
+        process. VisualizationSubprocess will create 
+        and visualize cls_app_to_visualize. Hot reloads
+        can be performed by putting reload instructions
+        onto the visualization_instructions queue. 
+        '''
+        new_process = multiprocessing.Process(
+            target=VisualizationSubprocess,
+            args=(self.visualization_instructions,)
+        )
+        new_process.start()
+        self.visualization_subprocess = new_process
+
+    def hot_reload(self, reload_instruction):
+        if not self._is_visualizing():
+            self._start_visualizing()
+        self.visualization_instructions.put(reload_instruction) 
 
 Factory.register('RootWidget', module='main')
 Factory.register('KivyVisualizer', module='kivyvisualizer')
 Factory.register('Toolbar', module='toolbar')
 Factory.register('FileToolbarGroup', module='toolbar')
 
-# Hot reloading kvlang for the root object is no issue at all
-# Reloading updated python code is a lot tricker. My attempts
-# to use importlib failed to update widget definitions. 
-# more research required.
-# I believe I need to reload the modules in the same original 
-# order, which will require caching a dependency graph
-from multiprocessing import Process, Queue
-from visualizationsubprocess import VisualizationSubprocess, HotReloadInstruction
-
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-class ExampleRoot(Button):
-    pass
-
-class ExampleChild(App):
-    def build(self):
-        return ExampleRoot()
-
-class UpdatedApp(App):
-    def __init__(self, *kwargs):
-        self.q = Queue()
-        super().__init__(*kwargs)
-
-    def build(self):
-        box = BoxLayout()
-        b = Button(text='Example')
-        t = TextInput()
-        b.bind(on_press=self.launchChild)
-        t.bind(text=self.textChanged)
-        box.add_widget(b)
-        box.add_widget(t)
-        return box
-
-    def launchChild(self, button):
-        p = Process(target=VisualizationSubprocess,args=(ExampleChild, self.q))
-        p.start() 
-
-    def textChanged(self, widget, text):
-        # Just setup to trigger a reload with each keypress for now
-        self.q.put(HotReloadInstruction("visualizationsubprocess"))
-
 if __name__ == '__main__':
-    #KivyDesignerApp().run()
-    UpdatedApp().run()
+    KivyDesignerApp().run()
--- src/visualizationsubprocess.py
+++ src/visualizationsubprocess.py
@@ -7,37 +7,71 @@ from kivy.app import App
 from functools import partial
 
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from enum import Enum, auto
 
 @dataclass
 class HotReloadInstruction:
-    kivy_build_str: str 
+    '''
+    Hot Reload Instruction set supported 
+    by the VisualizationSubprocess. The visualized
+    app can be directed by pushing one or more 
+    reload instructions onto the subprocess 
+    instruction queue. 
+    '''
+    kivy_build_str: str = ''
+
+class ListenerInstructions(Enum):
+    '''
+    Internal class used by VisualizationSubprocess
+    to control the listening task
+    '''
+    STOPLISTENING = auto()
+    '''Stop listening for reload instructions and complete task execution'''
 
 class HotReloadApp(App):
+    '''
+    Application used to host the visualized root widget. 
 
-    def __init__(self, build_func, *kwargs): 
-        self.build_func = build_func
+    Reloads are currently achieved by swapping the root widget.
+    '''
+
+    def __init__(self, *kwargs): 
+        self.build_func = Label
         self.registered_build_files = set()
         return super(HotReloadApp, self).__init__(*kwargs)
 
     def build(self):
+        '''
+        Unregister previously loaded builder files and 
+        build the root widget.
+        '''
+        for file in self.registered_build_files:
+            Builder.unload_file(file)
+        self.registered_build_files = set()
+
         files_before_build = set(Builder.files[:])
         new_root = self.build_func() 
         files_after_build = set(Builder.files[:])
+
         self.registered_build_files = files_after_build - files_before_build
         return new_root
   
     def update(self, build_func=None):
+        '''
+        Clear all widgets from the application window, unload all
+        files previously registered with the kivy Builder, and 
+        add a new root widget using the widget returned by 
+        build_func. 
+        '''
         if build_func:
             self.build_func = build_func 
-
-        for file in self.registered_build_files:
-            Builder.unload_file(file)
 
         for widget in Window.children[:]:
             Window.remove_widget(widget)
 
         try:
-            new_root = self.build_func()
+            new_root = self.build()
             Window.add_widget(new_root)
         except Exception as builderr:
             Window.add_widget(Label(text=str(builderr)))
@@ -47,22 +81,27 @@ class VisualizationSubprocess:
     This class is designed to perform a hot reload kivy 
     applications that are executing in a subprocess. 
 
-    The parent process can signal a reload by pushing an 
-    updated kvlang string onto the processing queue. The
+    The parent process can signal a reload by pushing a
+    hot reload instruction onto the processing queue. The
     processing queue should be populated with `HotReloadInstruction`s. 
     '''
-    def __init__(self, cls_app_to_visualize, hot_reload_queue_ref):       
+    def __init__(self, hot_reload_queue_ref):       
         self.reload_queue = hot_reload_queue_ref
-        self.listen_task = Thread(target=self.listen_for_updates)
-        self.reload_app = HotReloadApp(build_func=cls_app_to_visualize().build)
+        self.listen_task = Thread(target=self._listen_for_updates)
+        self.reload_app = HotReloadApp()
         
         self.listen_task.start()
-        self.reload_app.run()
+        try:
+            self.reload_app.run()
+        finally:
+            # Application is no longer running. Instruct
+            # the listening task to finish, and 
+            # wait for thread execution to complete
+            self.reload_queue.put(ListenerInstructions.STOPLISTENING)
+            self.listen_task.join()
+            self.listen_task = None
 
-    def __del__(self):
-        self.listen_task.join()
-
-    def listen_for_updates(self):
+    def _listen_for_updates(self):
         '''
         Wait until a hot reload request is available, then 
         apply the hot reload. This method will block the 
@@ -70,12 +109,26 @@ class VisualizationSubprocess:
         '''
         while True: 
             next_instruction = self.reload_queue.get()
-            Clock.schedule_once(partial(self.hot_reload, next_instruction))
 
-    def hot_reload(self, instruction, delta_time):
-        def b():
-            return Builder.load_string(instruction.kivy_build_str)
-        self.reload_app.update(build_func=b)
+            if next_instruction == ListenerInstructions.STOPLISTENING:
+                return
+
+            Clock.schedule_once(partial(self._hot_reload, next_instruction))
+
+    def _hot_reload(self, instruction, delta_time):
+        '''
+        Determine the correct handler for the provided hot reload 
+        instruction, and trigger the visualized app update. 
+
+        Hot reload handlers must provide an updated root widget.
+        '''
+        # Only one instuction is currently available
+        if instruction.kivy_build_str:
+            handler = lambda: Builder.load_string(instruction.kivy_build_str)
+        else:
+            # An empty build string was specified. Pass an empty widget
+            handler = Widget
+        self.reload_app.update(build_func=handler)
 
 
 
