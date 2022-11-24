a1b4801b7a03c01469eb5d6835c852927b5ff966
aMetallurgist <joshua.d1434@gmail.com>
2022-11-24 13:32:00 -0800

    Rename kivydesignerapp Let's try to keep module names simpler, to a max of two words if possible

--- kivydesigner/register_uix.py
+++ kivydesigner/register_uix.py
@@ -3,7 +3,7 @@ from kivy.factory import Factory
 # Register each of the custom widgets, to ensure they are available
 # to the kvlang builder
 
-Factory.register('RootWidget', module='kivydesigner.kivydesignerapp')
+Factory.register('RootWidget', module='kivydesigner.kivydesigner')
 Factory.register('IconButton', module='kivydesigner.uix.iconbutton')
 Factory.register('Toolbar', module='kivydesigner.uix.toolbar')
 Factory.register('FileToolbarGroup', module='kivydesigner.uix.toolbar')
