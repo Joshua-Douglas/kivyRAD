198e51c437d526ce0c3f8ca5ab2dd7ef0a75d10d
aMetallurgist <joshua.d1434@gmail.com>
2022-11-25 12:42:30 -0800

    Add images to file entries, to match the filetype We are currently showing kv files for everything. Before doing the full update I should add a resource manager that can find all of the required images for us.

--- kivydesigner/uix/kdfilechooser.py
+++ kivydesigner/uix/kdfilechooser.py
@@ -1,14 +1,25 @@
 from kivy.lang import Builder
 from kivy.uix.filechooser import FileChooserController, FileChooserLayout
+from kivy.uix.treeview import TreeViewNode
+from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_file('kdfilechooser_style.kv', rulesonly=True)
 
+class KDFilechooserEntry(BoxLayout, TreeViewNode):
+    
+    def get_entry_icon_path(self, is_dir, is_open):
+        if is_dir:
+            suffix = 'opened' if is_open else 'closed'
+            return f'atlas://data/images/defaulttheme/tree_{suffix}'
+        else:
+            return "C:\\Users\\joshu\\source\\repos\\kivydesigner\\kivydesigner\\data\\kivy-icon-32.png"
+
 class KDFilechooser(FileChooserController):
-    _ENTRY_TEMPLATE = 'KDFileListEntry'
+    _ENTRY_TEMPLATE = 'KDFilechooserEntryTemplate'
 
 class KDFilechooserLayout(FileChooserLayout):
     VIEWNAME = 'list'
-    _ENTRY_TEMPLATE = 'KDFileListEntry'
+    _ENTRY_TEMPLATE = 'KDFilechooserEntryTemplate'
 
     def __init__(self, **kwargs):
         super(KDFilechooserLayout, self).__init__(**kwargs)
@@ -19,4 +30,7 @@ class KDFilechooserLayout(FileChooserLayout):
 
 if __name__ == '__main__':
     from kivy.app import runTouchApp
-    runTouchApp(KDFilechooser())
+    from pathlib import Path
+
+    rootdir = Path(__file__).parent.parent
+    runTouchApp(KDFilechooser(rootpath=str(rootdir)))
